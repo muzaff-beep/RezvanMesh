200065f1c45e3985e1653b2257646a759a1b02fb
So Muzaffari <so.muzaff@gmail.com>
2026-04-23 12:29:42 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,16 +3,16 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.mozilla.org/maven2")   // ← Add this line
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.mozilla.org/maven2")   // ← Add this line
     }
 }
-
 rootProject.name = "RezvanMesh"
 include(":android:app")
