86de40aa01492f80ea51ef6099b58d399f489d72
So Muzaffari <so.muzaff@gmail.com>
2026-04-23 12:49:49 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,10 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    // Rust plugin removed – CI handles Rust build separately via cargo ndk
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
