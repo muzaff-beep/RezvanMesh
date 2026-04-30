0af37e06ce3fb6f3a181caa279fd9950f0bae350
So Muzaffari <so.muzaff@gmail.com>
2026-04-30 03:48:09 +0330

    Update sign.rs

--- rust/rezvan-crypto/src/sign.rs
+++ rust/rezvan-crypto/src/sign.rs
@@ -11,9 +11,9 @@ pub fn sign(identity: &IdentityKeypair, message: &[u8]) -> [u8; 64] {
 
 pub fn verify(public_key: &[u8; 32], message: &[u8], signature: &[u8; 64]) -> bool {
     let pk = sign::PublicKey(*public_key);
-    let sig = match sign::Signature::from_slice(signature) {
-        Some(s) => s,
-        None => return false,
+    let sig = match sign::Signature::from_bytes(signature) {
+        Ok(s) => s,
+        Err(_) => return false,
     };
     sign::verify_detached(&sig, message, &pk)
 }
