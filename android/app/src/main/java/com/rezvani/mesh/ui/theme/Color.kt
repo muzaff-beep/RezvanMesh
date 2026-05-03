fc61e2e59efe382e4142624e2431808257f5c145
So Muzaffari <so.muzaff@gmail.com>
2026-05-04 01:56:39 +0330

    Update Color.kt

--- android/app/src/main/java/com/rezvani/mesh/ui/theme/Color.kt
+++ android/app/src/main/java/com/rezvani/mesh/ui/theme/Color.kt
@@ -2,56 +2,33 @@ package com.rezvani.mesh.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Primary colors - Deep blue-gray (mature, military/technical feel)
-val PrimaryLight = Color(0xFF2C5F7C)
-val PrimaryDark = Color(0xFF1A3A4F)
-val OnPrimaryLight = Color(0xFFFFFFFF)
-val OnPrimaryDark = Color(0xFFE0E8EF)
+// ---- Dark Theme Colors ----
+val DarkBackground      = Color(0xFF060606)
+val DarkSurface         = Color(0xFF2A3036)
+val DarkSurfaceVariant  = Color(0xFF1A1D20)
+val DarkBorder          = Color(0xFF3A3F44)
 
-// Secondary colors - Warm amber (contrast, alerts)
-val SecondaryLight = Color(0xFFB8860B)
-val SecondaryDark = Color(0xFFD4A017)
-val OnSecondaryLight = Color(0xFFFFFFFF)
-val OnSecondaryDark = Color(0xFF1A1A1A)
+val PrimaryGreen        = Color(0xFF1E6B4E)
+val PrimaryGreenHover   = Color(0xFF25805A)
+val PrimaryGreenDark    = Color(0xFF1A5A3E)
 
-// Tertiary colors - Muted teal (information)
-val TertiaryLight = Color(0xFF2E7D6E)
-val TertiaryDark = Color(0xFF4A9B8A)
-val OnTertiaryLight = Color(0xFFFFFFFF)
-val OnTertiaryDark = Color(0xFFE0F0EC)
+val DangerRed           = Color(0xFFD32F2F)
+val SuccessGreen        = Color(0xFF4CAF50)
+val WarningOrange       = Color(0xFFFF9800)
 
-// Error colors - Strong red (emergency)
-val ErrorLight = Color(0xFFBA1A1A)
-val ErrorDark = Color(0xFFFF6B6B)
-val OnErrorLight = Color(0xFFFFFFFF)
-val OnErrorDark = Color(0xFF1A1A1A)
+val TextWhite           = Color(0xFFFFFFFF)
+val TextPrimaryDark     = Color(0xFFCDCFD0)
+val TextMutedDark       = Color(0xFF8A8F94)
+val TextDimDark         = Color(0xFF6A7075)
+val ErrorContainerDark  = DangerRed.copy(alpha = 0.15f)
 
-// Background colors
-val BackgroundLight = Color(0xFFF8FAFC)
-val BackgroundDark = Color(0xFF121820)
-val OnBackgroundLight = Color(0xFF1A242E)
-val OnBackgroundDark = Color(0xFFE2E8F0)
+// ---- Light Theme Colors ----
+val LightBackground     = Color(0xFFF5F5F5)
+val LightSurface        = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFE0E0E0)
+val LightBorder         = Color(0xFFC0C0C0)
 
-// Surface colors
-val SurfaceLight = Color(0xFFFFFFFF)
-val SurfaceDark = Color(0xFF1E293B)
-val OnSurfaceLight = Color(0xFF1A242E)
-val OnSurfaceDark = Color(0xFFE2E8F0)
-
-// Surface variant colors
-val SurfaceVariantLight = Color(0xFFE8EEF4)
-val SurfaceVariantDark = Color(0xFF2D3A4A)
-val OnSurfaceVariantLight = Color(0xFF41505F)
-val OnSurfaceVariantDark = Color(0xFFB0C4D9)
-
-// Outline colors
-val OutlineLight = Color(0xFF6F7E8C)
-val OutlineDark = Color(0xFF8A9BA8)
-
-// Additional custom colors for Rezvan Mesh
-val EmergencyRed = Color(0xFFD32F2F)
-val EmergencyRedDark = Color(0xFFFF5252)
-val PowerGreen = Color(0xFF2E7D32)
-val PowerYellow = Color(0xFFF9A825)
-val PowerOrange = Color(0xFFEF6C00)
-val MeshBlue = Color(0xFF1565C0)
+val TextPrimaryLight    = Color(0xFF212121)
+val TextMutedLight      = Color(0xFF757575)
+val TextDimLight        = Color(0xFF9E9E9E)
+val ErrorContainerLight = DangerRed.copy(alpha = 0.08f)
