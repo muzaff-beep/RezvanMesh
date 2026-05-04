50fd63e934141a739e2c7a8c528fc621257a63bc
So Muzaffari <so.muzaff@gmail.com>
2026-05-04 21:34:18 +0330

    Update RezvanRadioService.kt

--- android/app/src/main/java/com/rezvani/mesh/radio/RezvanRadioService.kt
+++ android/app/src/main/java/com/rezvani/mesh/radio/RezvanRadioService.kt
@@ -2,6 +2,7 @@ package com.rezvani.mesh.radio
 
 import android.app.Notification
 import android.app.NotificationChannel
+import com.rezvani.mesh.MeshServiceConnection
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
