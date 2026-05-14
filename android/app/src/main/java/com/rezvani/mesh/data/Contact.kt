981a9a4adac2aabcc556e8ca611caab67c9a983b
So Muzaffari <so.muzaff@gmail.com>
2026-05-14 20:36:50 +0330

    Update Contact.kt

--- android/app/src/main/java/com/rezvani/mesh/data/Contact.kt
+++ android/app/src/main/java/com/rezvani/mesh/data/Contact.kt
@@ -1,6 +1,8 @@
+// android/app/src/main/java/com/rezvani/mesh/data/Contact.kt
+
 package com.rezvani.mesh.data
 
 data class Contact(
     val name: String,
-    val nodeIdHex: String  // hex representation of NodeId (16 characters)
+    val nodeIdHex: String
 )
