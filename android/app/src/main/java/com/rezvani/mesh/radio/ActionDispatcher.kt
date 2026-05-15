56fae322144d07c0f6b66cdc7ee1af417a1a914d
So Muzaffari <so.muzaff@gmail.com>
2026-05-15 19:27:02 +0330

    Update ActionDispatcher.kt

--- android/app/src/main/java/com/rezvani/mesh/radio/ActionDispatcher.kt
+++ android/app/src/main/java/com/rezvani/mesh/radio/ActionDispatcher.kt
@@ -1,25 +1,8 @@
-// android/app/src/main/java/com/rezvani/mesh/ActionDispatcher.kt
+// android/app/src/main/java/com/rezvani/mesh/radio/ActionDispatcher.kt
 
-package com.rezvani.mesh
-
-import com.rezvani.mesh.rust.Action
-import com.rezvani.mesh.utils.DiagLogger
+package com.rezvani.mesh.radio
 
 object ActionDispatcher {
-    fun dispatch(action: Action, meshConnection: MeshServiceConnection? = null) {
-        when (action) {
-            is Action.SendBleAdvertisement -> {
-                // handled by RezvanRadioService tick
-            }
-            is Action.SendBlePacket -> {
-                // handled by RezvanRadioService tick
-            }
-            is Action.DiagLog -> {
-                DiagLogger.custom(action.tag, action.level, action.message)
-            }
-            else -> {
-                // future actions
-            }
-        }
-    }
+    // Action dispatch is now handled directly in RezvanRadioService via nativeTick/nativeProcessIncoming.
+    // This file remains as a placeholder for future refactoring.
 }
